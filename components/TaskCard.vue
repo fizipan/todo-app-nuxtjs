e90be3c9bee8518bd0dcae4d14baf726ca4ae7a6
hafizhmaulanay <56567633+hafizhmaulanay@users.noreply.github.com>
2022-08-22 12:14:48 +0700

    update

---
+++ components/TaskCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="`task ${task.done ? 'is-complete' : ''}`">
+    <div class="content">{{ task.content }}</div>
+    <div class="buttons">
+      <button @click="toggleDone">{{ task.done ? 'Undo' : 'Done' }}</button>
+      <button class="delete" @click="removeTask">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    toggleDone() {
+      this.$store.commit('TOGGLE_TASK', this.task)
+    },
+    removeTask() {
+      this.$store.commit('REMOVE_TASK', this.task)
+    },
+  },
+}
+</script>
+
+<style></style>
